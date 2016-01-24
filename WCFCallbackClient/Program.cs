9badbd52916e40b60ff985b0c5a18cc78c1b9330
Eduardo Brandao Barbosa <ebranbar@hotmail.com>
2016-01-23 22:23:40 -0200

    first version working - client to test the WCFCallBack.Server

---
+++ WCFCallbackClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading;
+using WCFCallbackClient.WCFCallback;
+
+namespace WCFCallbackClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RetornoServico callback = new RetornoServico();
+            InstanceContext context = new InstanceContext(callback);
+            var client = new ServicoClient(context);
+
+            while (!Console.KeyAvailable)
+            {
+                try
+                {
+                    client.IniciaServico();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error at service {0}", client.Endpoint.Address);
+                    Console.ResetColor();
+                    client.Abort();
+                    client = new ServicoClient(context);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WCFCallbackClient/RetornoServico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WCFCallbackClient.WCFCallback;
+
+namespace WCFCallbackClient
+{
+    internal class RetornoServico : IServicoCallback
+    {
+        public void RetornouServico(Retorno retorno)
+        {
+            Console.WriteLine("{0} number {1}.", retorno.Nome, retorno.Numero);
+        }
+    }
+}
